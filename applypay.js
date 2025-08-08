8e863ebff9b21938d5b065d40fb2197cd805a5bc
TJ <afzaltj81@gmail.com>
2025-08-08 09:10:49 -0400

    Update applypay.js

--- applypay.js
+++ applypay.js
@@ -20,6 +20,9 @@ function onApplePayButtonClicked() {
             "masterCard",
             "amex",
             "discover",
+            "ApplePay",
+            "interac",
+            "maestro"
         ],
         
         "total": {
@@ -83,6 +86,25 @@ function onApplePayButtonClicked() {
     session.oncancel = event => {
         // Payment canceled by WebKit
     };
+
+    // Begin the Apple Pay session
+    function ApplePaySession() {
+        return new ApplePaySession(request);
+       if (ApplePaySession === undefined) {
+            console.error("Apple Pay is not supported in this browser.");
+            return;
+        }
+        ApplePayNetworks = request.supportedNetworks;
+        ApplePayCapabilities = request.merchantCapabilities;
+        ApplePayCountryCode = request.countryCode;
+        ApplePayCurrencyCode = request.currencyCode;
+        ApplePayTotal = request.total.amount;
+        request.applePayButton = document.getElementById("apple-pay-button");
+        request.applePayButton.addEventListener("click", onApplePayButtonClicked);
+        request.applePayButton.style.display = "block";
+        request.ApplePayNetworks = ApplePayNetworks;
+    }
+    
     
     session.begin();
 }
