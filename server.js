fb10b53cc65ea5a9731df3d34b9c4af4a2a58aac
Tj <afzaltj81@gmail.com>
2025-11-02 08:00:37 -0800

    fixed index issues

--- server.js
+++ server.js
@@ -1,27 +1,3 @@
-// import express from 'express';
-// import path from 'path';
-// import { fileURLToPath } from 'url';
-
-// const __filename = fileURLToPath(import.meta.url);
-// const __dirname = path.dirname(__filename);
-
-// const app = express();
-// const PORT = 3000;
-
-// app.use(express.static(path.join(__dirname, 'public')));
-
-// app.get('/dev', (req, res) => {
-//   res.sendFile(path.join(__dirname, 'public', 'index.html'));
-// });
-
-// // Only start the server if run directly
-// if (import.meta.url === `file://${process.argv[1]}`) {
-//   app.listen(PORT, () => {
-//     console.log(` Server running at http://localhost:${PORT}/dev`);
-//   });
-// }
-
-// export default app;
 
 const express = require('express');
 const path = require('path');
