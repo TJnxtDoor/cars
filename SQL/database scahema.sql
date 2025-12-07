50329cf605a2d1b3eb330dc3ce01932023fbe537
Tj <afzaltj81@gmail.com>
2025-12-06 21:56:25 -0500

    i fogor 💀

--- SQL/database scahema.sql
+++ SQL/database scahema.sql
@@ -1,4 +1,4 @@
-CREATE TABLE customers (
+CREATE TABLE customers_id (
   customer_id SERIAL PRIMARY KEY,
   name VARCHAR(100),
   email VARCHAR(100) UNIQUE NOT NULL,
@@ -16,3 +16,15 @@ CREATE TABLE payment_methods (
   is_default BOOLEAN DEFAULT FALSE,
   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
+
+CREATE TABLE orders {
+  order_id SERIAL PRIMARY KEY,
+  customer_id INT REFERENCES customers(customer_id),
+  order_total DECIMAL(10, 2),
+  order_status VARCHAR(50),
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+};
+
+SELECT TABLE FROM payment_methods WHERE customer_id = 0 AND is_default = TRUE;
+FROM payment_methods SELECT * WHERE customer_id = 0 AND is_default = TRUE;
+
